8b323dc0a33e8f28c45232cfde53ecd8f450e3b9
Fakhri Hassan Maulana <87844859+dzzlr@users.noreply.github.com>
2025-04-27 11:13:05 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import fastapi
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {
+        "app": "FastAPI Healthcheck Demo",
+        "fastapi_version": fastapi.__version__
+    }
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
